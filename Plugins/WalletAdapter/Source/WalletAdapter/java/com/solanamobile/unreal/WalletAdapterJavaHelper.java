ee3285a62b3f02633b4507199429b478b754035b
Sergey Makovkin <makovkin.s@gmail.com>
2023-04-25 15:03:55 +0300

    Replaced solana java files with dependencies

---
+++ Plugins/WalletAdapter/Source/WalletAdapter/java/com/solanamobile/unreal/WalletAdapterJavaHelper.java
@@ -0,0 +1,23 @@
+// Copyright (c) Sergey Makovkin (makovkin.s@gmail.com). All Rights Reserved.
+
+package com.solanamobile.unreal;
+
+import com.solana.mobilewalletadapter.clientlib.protocol.JsonRpc20Client;
+import com.solana.mobilewalletadapter.clientlib.protocol.MobileWalletAdapterClient;
+import com.solana.mobilewalletadapter.clientlib.scenario.LocalAssociationIntentCreator;
+import com.solana.mobilewalletadapter.clientlib.scenario.LocalAssociationScenario;
+import com.solana.mobilewalletadapter.clientlib.scenario.Scenario;
+import com.solana.mobilewalletadapter.common.ProtocolContract;
+//import android.app.Activity;
+
+public final class WalletAdapterJavaHelper 
+{    
+    //MobileWalletAdapterClient client;
+    
+    //public WalletAdapterJavaHelper() 
+    //{
+        //client = new MobileWalletAdapterClient(0);        
+    //}
+
+    //public static native void onCreateSeed(boolean bSuccess, long authToken);
+}
