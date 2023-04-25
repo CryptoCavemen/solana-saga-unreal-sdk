f87eec5cf74498d5c5851b99a5a01d76b28cfdd2
Sergey Makovkin <makovkin.s@gmail.com>
2023-04-25 14:31:18 +0300

    Added missed "common" files

--- Plugins/WalletAdapter/Source/WalletAdapter/java/com/solana/unreal/WalletAdapterJavaHelper.java
+++ Plugins/WalletAdapter/Source/WalletAdapter/java/com/solana/unreal/WalletAdapterJavaHelper.java
@@ -1,9 +1,9 @@
 // Copyright (c) Sergey Makovkin (makovkin.s@gmail.com). All Rights Reserved.
 
-package com.solanamobile.unreal;
+package com.solana.unreal;
 //import android.app.Activity;
 
-public final class WalletJavaHelper 
+public final class WalletAdapterJavaHelper 
 {
     //private Activity mParent;
     //public WalletJavaHelper(Activity unrealActivity) 
